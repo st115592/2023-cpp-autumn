3567d88ff2a64382136e351e0e937ad94a388f21
st115592 <st115592@student.spbu.ru>
2023-09-14 20:33:53 +0300

    homework code update

--- 2023.09.09-homework-1/task2/main.cpp
+++ 2023.09.09-homework-1/task2/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int b;
     cin >> b;
-    cout << ((1 + b) * b) / 2;
+    cout << ((1 + b) * (abs(b - 1) + 1))/ 2;
 
     return 0;
 }
--- 2023.09.09-homework-1/task5/main.cpp
+++ 2023.09.09-homework-1/task5/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int a,b,c,d;
 
+    cin>> a >> b;
+
+    c = a * b % 109;
+
+    d = ( c + 109 ) % 109 + 1;
+
+    cout << d;
+
+    return 0;
+}
