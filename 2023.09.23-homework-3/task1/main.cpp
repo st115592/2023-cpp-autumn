4b653f66182c264e7888b4a8d27e7b4cd98e0781
st115592 <st115592@student.spbu.ru>
2023-10-01 23:15:50 +0300

    homework 2 code update 2

--- 2023.09.23-homework-3/task1/main.cpp
+++ 2023.09.23-homework-3/task1/main.cpp
@@ -2,19 +2,19 @@
 
 int main()
 {
-int n = 0;
-int a = 1;
-int k = 0;
-std::cin >> n;
-for (int i = 1; i <= n; i++)
-{
-    std::cout << a << " " ;
-    k++;
-    if (a == k)
-        {
-            a++;
-            k = 0;
-        }
-}
+ int n = 0;
+    int a = 1;
+    int k = 0;
+    std::cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        std::cout << a << " " ;
+        k++;
+        if (a == k)
+            {
+                a++
+                k = 0;
+            }
+    }
     return 0;
 }
