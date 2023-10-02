1757d1356059806f61a22f9428e93ba88d8cc2b4
st115592 <st115592@student.spbu.ru>
2023-10-02 16:16:46 +0300

    homework 3 code update 2

--- 2023.09.23-homework-3/task3/main.cpp
+++ 2023.09.23-homework-3/task3/main.cpp
@@ -6,13 +6,18 @@ int main()
     int i = 0;
     int k = 0;
     std::cin >> x;
-    for (i = 1; i <= x ;++i)
+    for (i = 1; i * i < x ;++i)
     {
         if (x % i==0)
         {
-            k++;
+            k += 2;
         }
     }
+    if (x == i*i)
+    {
+        k++;
+    }
     std::cout << k;
+
     return 0;
 }
